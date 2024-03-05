1ebc42162d93dc4ec33d4c87ad708b0af16481a0
Jeremy Badger <jeremebadger@gmail.com>
2024-03-05 08:35:22 -0800

    Static HTML implementation

---
+++ README.md
@@ -0,0 +1,12 @@
+1. static display - do styling, buttons, no buttons, etc
+2. derive from a state
+3. add handlers
+4. update the state
+
+Display of blanks (for now 7 blanks)
+
+Buttons for A-Z
+
+Using a button uncovers all instances of that letter in word
+
+image
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,64 @@
 import React from 'react'
 
+const step = 6
+
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <div>
+      <main>
+        <header>
+          <h1>Snowman</h1>
+          <h2>A Hangman-style game</h2>
+        </header>
+        <section id="game-board">
+          <div className="results-area">
+            <div className="snowman-area">
+              <div className={`snowman-pic${step}`} />
+            </div>
+            <div className="revealed-area">
+              <ul className="letters-revealed">
+                <li className="pressed">C</li>
+                <li className="pressed">A</li>
+                <li className="pressed">U</li>
+                <li className="pressed">T</li>
+                <li>_</li>
+                <li className="pressed">O</li>
+                <li className="pressed">N</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section id="alphabet">
+          <ul className="letters-list">
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+            <li>D</li>
+            <li>E</li>
+            <li>F</li>
+            <li>G</li>
+            <li>H</li>
+            <li>I</li>
+            <li>J</li>
+            <li>K</li>
+            <li>L</li>
+            <li>M</li>
+            <li>N</li>
+            <li>O</li>
+            <li>P</li>
+            <li>Q</li>
+            <li>R</li>
+            <li>S</li>
+            <li>T</li>
+            <li>U</li>
+            <li>V</li>
+            <li>W</li>
+            <li>X</li>
+            <li>Y</li>
+            <li>Z</li>
+          </ul>
+        </section>
+      </main>
+    </div>
+  )
 }
