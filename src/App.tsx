6c66ccfbf2c7e5dea568cad9ffb157bc65817cec
Jeremy Badger <jeremebadger@gmail.com>
2024-03-11 15:44:35 -0700

    Added stylized buttons, fonts, and media query

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react'
+import FadeIn from 'react-fade-in'
+import { v4 as uuidv4 } from 'uuid'
+
+import step_0 from '../images/transparent_images/step_0.png'
+import step_1 from '../images/transparent_images/step_1.png'
+import step_2 from '../images/transparent_images/step_2.png'
+import step_3 from '../images/transparent_images/step_3.png'
+import step_4 from '../images/transparent_images/step_4.png'
+import step_5 from '../images/transparent_images/step_5.png'
+import step_6 from '../images/transparent_images/step_6.png'
+import step_7 from '../images/transparent_images/step_7.png'
+import snow from '../images/transparent_images/snow.gif'
 import words from './words.json'
 
-// should this go here/is this how i should have this?
-let step = 0
-
+const testMode = true
 type Board = string[]
-
 type Game = {
   letter: null | string
   word: string[]
   board: Board
   state: string | null
-  // state: null | undefined | 'new' | 'playing' | 'won'
-  numOfCorrectLetters: null | number
+  numOfCorrectLetters: number
 }
 
 const initialState = {
@@ -30,65 +38,121 @@ export function App() {
     board: ['_', '_', '_', '_', '_', '_', '_'],
     state: null,
     numOfCorrectLetters: 0,
-    // numOfLetter:7
   })
 
+  const letters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ]
+
+  const snowmanPicArray = [
+    step_0,
+    step_1,
+    step_2,
+    step_3,
+    step_4,
+    step_5,
+    step_6,
+    step_7,
+  ]
+
+  const [snowmanPicCount, setSnowmanPicCount] = useState(7)
+
+  // useEffect(() => {
+  //   // Generate unique IDs for list items
+  //   const updatedItems = Array(5).fill().map(() => ({
+  //     id: crypto.randomUUID(),
+  //     name: 'Example Item',
+  //   }));
+  //   setItems(updatedItems);
+  // }, []);
+
   function handleClickABCButton(value: string) {
+    let count = 0
+    disableButton(value)
+
     if (game.state === null) {
       return
     }
-    disableButton(value)
-
-    let count = 0
-
-    console.log(`Before: ${game.board}`)
-
-    // const newGameBoard = { ...game }
     const tempBoard = [...game.board]
     if (game.word.includes(value)) {
       for (let i in game.word) {
         if (game.word[i] === value) {
           tempBoard[i] = game.word[i]
           count++
-          console.log(game.word)
-          console.log(tempBoard)
         }
       }
     }
-    step += count
 
     let tempState = gameStatus(game.state, tempBoard)
+    setSnowmanPicCount(game.numOfCorrectLetters + count)
 
     setGame({
       ...game,
       letter: value,
       board: tempBoard,
-      numOfCorrectLetters: step,
+      numOfCorrectLetters: game.numOfCorrectLetters + count,
       state: tempState!,
     })
-    consoleMessages()
+
+    if (testMode === true) {
+      consoleMessages()
+    }
   }
 
   async function handleNewGame() {
+    document.getElementById('iframeWrapper')?.classList.add('won')
+
+    setSnowmanPicCount(0)
     enableButtons()
     const newGame = { ...initialState }
     let tempWord = words[Math.floor(Math.random() * 1024)]
       .toUpperCase()
       .split('')
-    // game.word = ['L', 'E', 'I', 'E', 'G', 'H', 'A']
     setGame({ ...newGame, word: tempWord, state: 'new' })
-    console.log(`The word is ${tempWord.join('')}`)
-    console.log(`The board has ${newGame.board.join(' ')}`)
+
+    if (testMode === true) {
+      //////////////////////////////////////////////////////////
+      // Testing - New game console messages to expedite finishing game
+      console.log(`The word is ${tempWord.join('')}`)
+      console.log(`The board has ${newGame.board.join(' ')}`)
+    }
   }
 
   function gameStatus(status: string | null, board: Array<String>) {
     if (status !== null && board.includes('_')) {
       return 'playing'
     } else if (status === 'playing' && !board.includes('_')) {
+      document.getElementById('iframeWrapper')?.classList.remove('won')
       return 'won'
     }
   }
 
+  //////////////////////////////////////////////////////////
   // Disabling/Re-enabling seems to be working fine
   function disableButton(letter: string) {
     const button = document.getElementById(letter)
@@ -102,6 +166,8 @@ export function App() {
     })
   }
 
+  //////////////////////////////////////////////////////////
+  // Testing - Game console messages to expedite finishing game
   function consoleMessages() {
     console.log(`The letter is ${game.letter}`)
     console.log(`The word is ${game.word.join('')}`)
@@ -110,8 +176,15 @@ export function App() {
     console.log(`The numOfCorrectLetters is ${game.numOfCorrectLetters}`)
   }
 
-  //function for 3 different button labels:
-  // New Game, Restart Game, Play Again?
+  function buttonLabel() {
+    if (game.state === null) {
+      return 'New Game'
+    } else if (game.state === 'new' || game.state === 'playing') {
+      return 'Restart Game'
+    } else {
+      return 'Play Again?'
+    }
+  }
 
   return (
     <div>
@@ -119,119 +192,43 @@ export function App() {
         <header>
           <h1>Snowman</h1>
           <h2>A Hangman-style game</h2>
-          {/* Needs to be fixed to properly display for button */}
-          <button onClick={handleNewGame}>
-            {game.state === null ||
-            game.state === 'new' ||
-            game.state === 'playing'
-              ? 'New Game'
-              : 'Play Again?'}
+          <button id="gameButton" onClick={handleNewGame}>
+            {buttonLabel()}
           </button>
         </header>
         <section id="game-board">
           <div className="results-area">
-            <div className="snowman-area">
-              <div className={`snowman-pic${game.numOfCorrectLetters}`} />
-            </div>
-            <div className="revealed-area">
-              <ul className="letters-revealed">
-                {/* <li className={game.board[0] === '_' ? undefined : 'revealed'}>
-                  {game.board[0]}
-                </li> */}
-                <li>{game.board[0]}</li>
-                <li>{game.board[1]}</li>
-                <li>{game.board[2]}</li>
-                <li>{game.board[3]}</li>
-                <li>{game.board[4]}</li>
-                <li>{game.board[5]}</li>
-                <li>{game.board[6]}</li>
-              </ul>
-            </div>
+            <FadeIn delay={150} transitionDuration={700}>
+              <div className="snowman-area">
+                <iframe src={snow} className="gif"></iframe>
+                <img
+                  src={snowmanPicArray[snowmanPicCount]}
+                  alt="Image of snowman"
+                />
+              </div>
+              <div className="revealed-area">
+                <ul className="letters-revealed">
+                  {game.board.map((boardIndex) => {
+                    return <li key={uuidv4()}>{boardIndex}</li>
+                  })}
+                </ul>
+              </div>
+            </FadeIn>
           </div>
         </section>
         <section id="alphabet">
-          <ul className="letters-list">
-            {/* <button onClick={() => handleClickABCButton(0)}>A</button> */}
-            {/* Cannot pass just a string/value, like 'A'??? */}
-            <button id="A" onClick={() => handleClickABCButton('A')}>
-              A
-            </button>
-            <button id="B" onClick={() => handleClickABCButton('B')}>
-              B
-            </button>
-            <button id="C" onClick={() => handleClickABCButton('C')}>
-              C
-            </button>
-            <button id="D" onClick={() => handleClickABCButton('D')}>
-              D
-            </button>
-            <button id="E" onClick={() => handleClickABCButton('E')}>
-              E
-            </button>
-            <button id="F" onClick={() => handleClickABCButton('F')}>
-              F
-            </button>
-            <button id="G" onClick={() => handleClickABCButton('G')}>
-              G
-            </button>
-            <button id="H" onClick={() => handleClickABCButton('H')}>
-              H
-            </button>
-            <button id="I" onClick={() => handleClickABCButton('I')}>
-              I
-            </button>
-            <button id="J" onClick={() => handleClickABCButton('J')}>
-              J
-            </button>
-            <button id="K" onClick={() => handleClickABCButton('K')}>
-              K
-            </button>
-            <button id="L" onClick={() => handleClickABCButton('L')}>
-              L
-            </button>
-            <button id="M" onClick={() => handleClickABCButton('M')}>
-              M
-            </button>
-            <button id="N" onClick={() => handleClickABCButton('N')}>
-              N
-            </button>
-            <button id="O" onClick={() => handleClickABCButton('O')}>
-              O
-            </button>
-            <button id="P" onClick={() => handleClickABCButton('P')}>
-              P
-            </button>
-            <button id="Q" onClick={() => handleClickABCButton('Q')}>
-              Q
-            </button>
-            <button id="R" onClick={() => handleClickABCButton('R')}>
-              R
-            </button>
-            <button id="S" onClick={() => handleClickABCButton('S')}>
-              S
-            </button>
-            <button id="T" onClick={() => handleClickABCButton('T')}>
-              T
-            </button>
-            <button id="U" onClick={() => handleClickABCButton('U')}>
-              U
-            </button>
-            <button id="V" onClick={() => handleClickABCButton('V')}>
-              V
-            </button>
-            <button id="W" onClick={() => handleClickABCButton('W')}>
-              W
-            </button>
-            <button id="X" onClick={() => handleClickABCButton('X')}>
-              X
-            </button>
-            <button id="Y" onClick={() => handleClickABCButton('Y')}>
-              Y
-            </button>
-            <button id="Z" onClick={() => handleClickABCButton('Z')}>
-              Z
-            </button>
-          </ul>
+          {letters.map((letter) => {
+            return (
+              <button
+                key={letter.toString()}
+                id={letter}
+                className="abcLetter"
+                onClick={() => handleClickABCButton(letter)}
+              >
+                {letter}
+              </button>
+            )
+          })}
         </section>
       </main>
     </div>
